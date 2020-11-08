f7d25233d0bf7b0f2471e1e4b118aa68408603cd
Gustavo Allen <gustavoallen92@gmail.com>
2020-11-08 09:03:05 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,7 +28,7 @@ class TicTacToe
   end
 
   def valid_move?(index)
-    if index.between?(0, 8) || !position_taken?(index)
+    if index.between?(0, 8) && !position_taken?(index)
       return true
     else
       return false
